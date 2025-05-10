b8a5dc493554eb2f80f7b18aff854a8c3aee898b
wonjunLee123 <opeveben@gmail.com>
2025-05-11 02:56:30 +0900

    뉴 씬

--- Assets/wonwong.cs
+++ Assets/wonwong.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 涝仿 贸府
+        movement.x = Input.GetAxisRaw("Horizontal"); // A, D
+        movement.y = Input.GetAxisRaw("Vertical");   // W, S
+    }
+
+    void FixedUpdate()
+    {
+        // 拱府 捞悼 贸府
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }
